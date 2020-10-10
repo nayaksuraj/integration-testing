c5a53f47768fa9d3cc960239dd0909c8773d8510
Suraj Nayak <34297372+nayaksuraj@users.noreply.github.com>
2020-10-10 19:59:10 +0530

    Update startDb.sh

--- startDb.sh
+++ startDb.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 docker stop demo-postgres
 docker rm demo-postgres
 docker run --name demo-postgres -p 15432:5432 -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=testuser -d postgres
